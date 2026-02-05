1fd73b2c37cab0edb6005f8902e63fbd08e76c05
Baakeleng <baakelengmaja@gmail.com>
2026-02-05 08:40:58 +0200

    adding comments for clear understanding

--- toster/src/comps/Tost.jsx
+++ toster/src/comps/Tost.jsx
@@ -2,27 +2,27 @@ import { useEffect, useRef, useState } from "react";
 
 const TOAST_TIMER = 45;
 
-//when you press the toast button
-export default function Tosting() {
-  const [isToast, setIsToasting] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(0);
 
-//To store timers for later use.
-  const intervalRef = useRef(null);
-  const timeoutRef = useRef(null);
+export default function Tosting() { //definines the toasting component
+  const [isToast, setIsToasting] = useState(false);//tracks if the toast is active initially or not
+  const [timeLeft, setTimeLeft] = useState(0);//tracks seconds left in the countdown
+
+//To store timers IDs for later use.
+  const intervalRef = useRef(null); // for countdown timer
+  const timeoutRef = useRef(null);//for dismissing the toast
 
 //use to stop the timer
   const clearTimers = () => {
     clearInterval(intervalRef.current);
     clearTimeout(timeoutRef.current);
   };
-
+//functions to handle stop timers and reset state
   const toastPopUp = () => {
     clearTimers();
     setIsToasting(false);
     setTimeLeft(0);
   };
-//for when you start the toast button
+//start the toast to set up countdown and auto-dsimiss
   const startToast = () => {
     if (isToast) return;
 
@@ -35,15 +35,15 @@ export default function Tosting() {
 
     timeoutRef.current = setTimeout(toastPopUp, TOAST_TIMER * 1000);
   };
-
+//cancells the toast
   const cancelToast = () => {
     toastPopUp();
   };
-
+//cleans up timers 
   useEffect(() => {
     return () => clearTimers();
   }, []);
-
+//jsx shows status,countdown and buttons
   return (
     <div>
       <p>Status: <b>{isToast ? "Toasting..." : "Off"}</b></p>
